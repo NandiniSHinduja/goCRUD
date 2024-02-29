9cc262efedc86f9de07108400391557c51a1dcde
Nandini S Hinduja <69416051+NandiniSHinduja@users.noreply.github.com>
2024-02-29 17:02:01 +0530

    Update book.go

--- CRUD/router/book.go
+++ CRUD/router/book.go
@@ -1,181 +1,162 @@
-package router
+package main
 
 import (
-	"github.com/bmdavis419/fiber-mongo-example/common"
+	"encoding/json"
+	"errors"
+	"golang-fiber-crud/common"
+	"golang-fiber-crud/router"
+	"os"
+
 	"github.com/bmdavis419/fiber-mongo-example/models"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
-func AddBookGroup(app *fiber.App) {
-	bookGroup := app.Group("/books")
+func main() {
+	err := run()
 
-	bookGroup.Get("/", getBooks)
-	bookGroup.Get("/:id", getBook)
-	bookGroup.Post("/", createBook)
-	bookGroup.Put("/:id", updateBook)
-	bookGroup.Delete("/:id", deleteBook)
-}
-
-func getBooks(c *fiber.Ctx) error {
-	coll := common.GetDBCollection("books")
-
-	// find all books
-	books := make([]models.Book, 0)
-	cursor, err := coll.Find(c.Context(), bson.M{})
 	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-		})
+		panic(err)
 	}
-
-	// iterate over the cursor
-	for cursor.Next(c.Context()) {
-		book := models.Book{}
-		err := cursor.Decode(&book)
-		if err != nil {
-			return c.Status(500).JSON(fiber.Map{
-				"error": err.Error(),
-			})
-		}
-		books = append(books, book)
-	}
-
-	return c.Status(200).JSON(fiber.Map{"data": books})
 }
 
-func getBook(c *fiber.Ctx) error {
-	coll := common.GetDBCollection("books")
+func run() error {
+	// init env
+	err := common.LoadEnv()
+	if err != nil {
+		return err
+	}
 
-	// find the book
+	// init db
+	err = common.InitDB()
+	if err != nil {
+		return err
+	}
+
+	err = router.InitDB2()
+	if err != nil {
+		return err
+	}
+
+	// defer closing db
+	defer common.CloseDB()
+
+	// create app
+	app := fiber.New()
+
+	// add basic middleware
+	app.Use(logger.New())
+	app.Use(recover.New())
+	app.Use(cors.New())
+
+	// add routes
+	router.AddBookGroup(app)
+	AddtitlesGroup(app)
+
+	// router.AddtitlesGroup(app)
+
+	// start server
+	var port string
+	if port = os.Getenv("PORT"); port == "" {
+		port = "8080"
+	}
+	app.Listen(":" + port)
+
+	return nil
+}
+
+var redisClient *redis.Client
+
+func InitDB2() error {
+	uri2 := os.Getenv("REDIS_URI")
+	if uri2 == "" {
+		return errors.New("you must set your 'REDIS_URI' environmental variable")
+
+	}
+	redisClient = redis.NewClient(&redis.Options{
+		Addr: uri2, // Replace with your Redis server address and port
+	})
+	return nil
+
+}
+
+func AddtitlesGroup(app *fiber.App) {
+	bookGroup := app.Group("/titles")
+
+	bookGroup.Get("/", getstitles)
+	bookGroup.Get("/:id", getsTitle)
+	// bookGroup.Post("/", createBook)
+	// bookGroup.Put("/:id", updateBook)
+	// bookGroup.Delete("/:id", deleteBook)
+}
+
+func getstitles(c *fiber.Ctx) error {
+	// Get all book keys from a Redis Set named "titles"
+	keys, err := redisClient.SMembers(c.Context(), "titles").Result()
+	if err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	// Create an empty slice to store titles
+	titles := make([]models.Book, 0)
+
+	// Loop through each key and get the corresponding book data using HGETALL
+	for _, key := range keys {
+		val, err := redisClient.HGetAll(c.Context(), key).Result()
+		if err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		// Convert the map[string]string to a Book struct
+		book := models.Book{}
+		if err := json.Unmarshal([]byte(val["title"]), &book.Title); err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+		if err := json.Unmarshal([]byte(val["author"]), &book.Author); err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+		if err := json.Unmarshal([]byte(val["year"]), &book.Year); err != nil {
+			return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+		}
+
+		titles = append(titles, book)
+	}
+
+	return c.Status(200).JSON(fiber.Map{"data": titles})
+}
+
+func getsTitle(c *fiber.Ctx) error {
 	id := c.Params("id")
 	if id == "" {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "id is required",
-		})
-	}
-	objectId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "invalid id",
-		})
+		return c.Status(400).JSON(fiber.Map{"error": "id is required"})
 	}
 
+	// Use HGETALL to retrieve all fields for the book with the given ID
+	val, err := redisClient.HGetAll(c.Context(), "book:"+id).Result()
+	if err != nil {
+		if err == redis.Nil {
+			return c.Status(404).JSON(fiber.Map{"error": "book not found"})
+		}
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+
+	// Convert the map[string]string to a Book struct
 	book := models.Book{}
-
-	err = coll.FindOne(c.Context(), bson.M{"_id": objectId}).Decode(&book)
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error": err.Error(),
-		})
+	if err := json.Unmarshal([]byte(val["title"]), &book.Title); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+	if err := json.Unmarshal([]byte(val["author"]), &book.Author); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
+	}
+	if err := json.Unmarshal([]byte(val["year"]), &book.Year); err != nil {
+		return c.Status(500).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	return c.Status(200).JSON(fiber.Map{"data": book})
 }
 
-type createDTO struct {
-	Title  string `json:"title" bson:"title"`
-	Author string `json:"author" bson:"author"`
-	Year   string `json:"year" bson:"year"`
-}
-
-func createBook(c *fiber.Ctx) error {
-	// validate the body
-	b := new(createDTO)
-	if err := c.BodyParser(b); err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "Invalid body",
-		})
-	}
-
-	// create the book
-	coll := common.GetDBCollection("books")
-	result, err := coll.InsertOne(c.Context(), b)
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error":   "Failed to create book",
-			"message": err.Error(),
-		})
-	}
-
-	// return the book
-	return c.Status(201).JSON(fiber.Map{
-		"result": result,
-	})
-}
-
-type updateDTO struct {
-	Title  string `json:"title,omitempty" bson:"title,omitempty"`
-	Author string `json:"author,omitempty" bson:"author,omitempty"`
-	Year   string `json:"year,omitempty" bson:"year,omitempty"`
-}
-
-func updateBook(c *fiber.Ctx) error {
-	// validate the body
-	b := new(updateDTO)
-	if err := c.BodyParser(b); err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "Invalid body",
-		})
-	}
-
-	// get the id
-	id := c.Params("id")
-	if id == "" {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "id is required",
-		})
-	}
-	objectId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "invalid id",
-		})
-	}
-
-	// update the book
-	coll := common.GetDBCollection("books")
-	result, err := coll.UpdateOne(c.Context(), bson.M{"_id": objectId}, bson.M{"$set": b})
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error":   "Failed to update book",
-			"message": err.Error(),
-		})
-	}
-
-	// return the book
-	return c.Status(200).JSON(fiber.Map{
-		"result": result,
-	})
-}
-
-func deleteBook(c *fiber.Ctx) error {
-	// get the id
-	id := c.Params("id")
-	if id == "" {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "id is required",
-		})
-	}
-	objectId, err := primitive.ObjectIDFromHex(id)
-	if err != nil {
-		return c.Status(400).JSON(fiber.Map{
-			"error": "invalid id",
-		})
-	}
-
-	// delete the book
-	coll := common.GetDBCollection("books")
-	result, err := coll.DeleteOne(c.Context(), bson.M{"_id": objectId})
-	if err != nil {
-		return c.Status(500).JSON(fiber.Map{
-			"error":   "Failed to delete book",
-			"message": err.Error(),
-		})
-	}
-
-	return c.Status(200).JSON(fiber.Map{
-		"result": result,
-	})
-}
+// type createDTO struct {
+// 	Title
