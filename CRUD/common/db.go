f878ec1389a8eddd8ea1780f48418a5af7dd79fa
Nandini S Hinduja <69416051+NandiniSHinduja@users.noreply.github.com>
2024-02-29 15:51:02 +0530

    Update db.go

--- CRUD/common/db.go
+++ CRUD/common/db.go
@@ -3,7 +3,8 @@ package common
 import (
 	"context"
 	"errors"
-	"fmt"
+
+	// "fmt"
 	"os"
 
 	"github.com/go-redis/redis/v8"
@@ -22,7 +23,6 @@ func GetDBCollection(col string) *mongo.Collection {
 
 func InitDB() error {
 	uri := os.Getenv("MONGODB_URI")
-	uri2 := os.Getenv("REDIS_URI")
 
 	if uri == "" {
 		return errors.New("you must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
@@ -31,36 +31,8 @@ func InitDB() error {
 	if err != nil {
 		return err
 	}
-	if uri2 == "" {
-		return errors.New("you must set your 'REDIS_URI' environmental variable")
-
-	}
-	// client2, err2 := redis.NewClient(context.Background(),options.Client().ApplyURI(uri2))
-	// if err2!=nil{
-	// 	return err
-	// }
 
 	db = client.Database("go_demo")
-	// db2=client2.Ping("go_demo")
-
-	client2 := redis.NewClient(&redis.Options{
-		Addr: uri2,
-	})
-
-	// Check for connection errors
-	ctx := context.Background()
-	err2 := client2.Ping(ctx).Err()
-	if err2 != nil {
-		fmt.Println("Error connecting to Redis:", err2)
-		return err
-	}
-
-	err3 := client2.Set(ctx, "key", "value", 0).Err()
-	if err3 != nil {
-		fmt.Println("Error setting string:", err)
-	}
-
-	fmt.Println("Connected to Redis successfully!")
 
 	return nil
 }
