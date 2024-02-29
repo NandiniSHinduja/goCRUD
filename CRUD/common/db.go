088ed6203221abf6b37ef68c2a9ab5c1af10c486
Nandini S Hinduja <69416051+NandiniSHinduja@users.noreply.github.com>
2024-02-29 16:27:10 +0530

    Update db.go

--- CRUD/common/db.go
+++ CRUD/common/db.go
@@ -3,19 +3,13 @@ package common
 import (
 	"context"
 	"errors"
-
-	// "fmt"
 	"os"
 
-	"github.com/go-redis/redis/v8"
-
-	// "github.com/redis/go-redis/v9"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var db *mongo.Database
-var db2 *redis.Client
 
 func GetDBCollection(col string) *mongo.Collection {
 	return db.Collection(col)
@@ -23,7 +17,6 @@ func GetDBCollection(col string) *mongo.Collection {
 
 func InitDB() error {
 	uri := os.Getenv("MONGODB_URI")
-
 	if uri == "" {
 		return errors.New("you must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
